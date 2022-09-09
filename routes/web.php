85d9b5dd5c41ab5550ee3237af0904a4f2723808
H Domain <hostingdomain2019@gmail.com>
2022-09-09 13:16:26 +0700

    feat: integration detail movie

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
+use App\Http\Controllers\User\MovieController;
 use App\Http\Controllers\User\DashboardController;
 
 /*
@@ -29,6 +30,7 @@ Route::redirect('/', '/login');
 
 Route::middleware(['auth', 'role:user'])->prefix('dashboard')->name('user.dashboard.')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('index');
+    Route::get('movie/{movie:slug}', [MovieController::class, 'show'])->name('movie.show');
 });
 
 Route::prefix('prototype')->name('prototype.')->group(function () {
